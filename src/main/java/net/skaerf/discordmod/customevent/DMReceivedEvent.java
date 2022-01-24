1c807c5b5fd680fa41abb6610309cd8510391940
skaerf <lawrence.harrison@outlook.com>
2022-01-24 16:34:11 +0000

    DMReceivedEvent base

---
+++ src/main/java/net/skaerf/discordmod/customevent/DMReceivedEvent.java
@@ -0,0 +1,11 @@
+package net.skaerf.discordmod.customevent;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class DMReceivedEvent extends Event {
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
