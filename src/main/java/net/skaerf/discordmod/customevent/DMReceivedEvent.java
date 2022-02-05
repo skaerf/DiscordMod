cf765cc05016e5b8676427fbaf207b3c0aec1ba7
skaerf <lawrence.harrison@outlook.com>
2022-02-05 18:38:36 +0000

    getMinecraftUUID in DMReceivedEvent

--- src/main/java/net/skaerf/discordmod/customevent/DMReceivedEvent.java
+++ src/main/java/net/skaerf/discordmod/customevent/DMReceivedEvent.java
@@ -1,8 +1,16 @@
 package net.skaerf.discordmod.customevent;
 
 import net.skaerf.discordmod.Bot;
+import net.skaerf.discordmod.ConfigManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 public class DMReceivedEvent extends Event {
 
@@ -30,4 +38,18 @@ public class DMReceivedEvent extends Event {
     public void respond(String response) {
         Bot.sendMessageToUser(userID, response);
     }
+
+    public UUID getMinecraftUUID() {
+        UUID uuid = null;
+        List<String> linkedAccounts = ConfigManager.getDataFile().getStringList("linked-accounts");
+        for (String linkedAccount : linkedAccounts) {
+            String[] uuidAndId = linkedAccount.split(":");
+            if (Objects.equals(uuidAndId[1], this.userID)) {
+                uuid = UUID.fromString(uuidAndId[1]);
+                break;
+            }
+        }
+        return uuid;
+    }
+
 }
